f95920394542e44f6ec4ac0d7a630154dee53ced
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_BindingToMSAccessDb/Window1.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DXGrid_BindingToMSAccessDb {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = 
+                new nwindCustomerDataSetTableAdapters.CustomersTableAdapter().GetData();
+        }
+    }
+}
