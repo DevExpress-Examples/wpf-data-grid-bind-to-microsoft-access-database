7fc1a5c029852dbdf900b223d2cb7daf420e66b5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXGrid_BindingToMSAccessDb/Window1.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows
+
+Namespace DXGrid_BindingToMSAccessDb
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            grid.DataSource = (New nwindCustomerDataSetTableAdapters.CustomersTableAdapter()).GetData()
+        End Sub
+    End Class
+End Namespace
