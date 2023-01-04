7ffc50fa531385f020d71746bd23ba7c99068ac5
DevExpressExampleBot <fakeEmail>
2023-01-04 15:25:28 +0400

    Source auto update [skip ci]

--- VB/DXGrid_BindingToMSAccessDb/Window1.xaml.vb
+++ VB/DXGrid_BindingToMSAccessDb/Window1.xaml.vb
@@ -1,12 +1,13 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace DXGrid_BindingToMSAccessDb
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = (New nwindCustomerDataSetTableAdapters.CustomersTableAdapter()).GetData()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New nwindCustomerDataSetTableAdapters.CustomersTableAdapter().GetData()
         End Sub
     End Class
 End Namespace
